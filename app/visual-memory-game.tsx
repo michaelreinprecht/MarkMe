7e39064c3c7a9a45117f5dded30f633347db7e09
michaelreinprecht <reinprecht.michael.business@gmail.com>
2024-11-15 09:48:37 +0100

    Revamped memory card, basic setup for visual-memory game (wincon still broken)

--- app/visual-memory-game.tsx
+++ app/visual-memory-game.tsx
@@ -6,54 +6,57 @@ import MemoryCard from "../components/MemoryCard";
 import LevelIndicator from "../components/LevelIndicator";
 import PageTitle from "../components/PageTitle";
 
-export default function SequenceMemory() {
+export default function VisualMemory() {
   const [level, setLevel] = useState(0);
-  const [sequence, setSequence] = useState<number[]>([]);
+  const [pattern, setPattern] = useState<number[]>([]);
   const [clickedCards, setClickedCards] = useState<number[]>([]);
-  const [isSequenceCorrect, setIsSequenceCorrect] = useState<boolean>(true);
+  const [isPatternCorrect, setIsPatternCorrect] = useState<boolean>(true);
   const [highlightedCards, setHighlightedCards] = useState<number[]>([]);
   const [inputEnabled, setInputEnabled] = useState<boolean>(false);
-  const highlightDuration = 500;
-  const gridSize = 3;
+  const highlightDuration = 2000;
+  const [gridSize, setGridSize] = useState(3); // Initialize gridSize
   const router = useRouter();
-  const title = "Sequence Memory";
+  const title = "Visual Memory";
 
-  const generateNextRandomNumber = (lastNumber: number | undefined) => {
+  const generateNextRandomNumber = (currentPattern: number[]) => {
     let newNumber;
     do {
-      newNumber = Math.floor(Math.random() * gridSize ** 2); // Generate a number between 0 and 8
-    } while (newNumber === lastNumber); // Ensure it's not the same as the last number
+      newNumber = Math.floor(Math.random() * gridSize ** 2); // Generate a number in range of the grid
+    } while (currentPattern.includes(newNumber)); // Ensure we only generate unique numbers for the pattern
     return newNumber;
   };
 
+  const generateRandomPattern = () => {
+    let newPattern: number[] = [];
+    while (newPattern.length < level + 3) {
+      newPattern.push(generateNextRandomNumber(newPattern));
+    }
+    return newPattern;
+  };
+
+  const getGridSize = (level: number) => {
+    return Math.floor(level / 3) + 3; // Increase grid size every 3 levels
+  };
+
   useEffect(() => {
-    setSequence((prevSequence) => {
-      const lastNumber = prevSequence[prevSequence.length - 1];
-      const newNumber = generateNextRandomNumber(lastNumber);
-      return [...prevSequence, newNumber];
-    });
+    setGridSize(getGridSize(level));
+    setPattern(generateRandomPattern());
   }, [level]);
 
   useEffect(() => {
     // Highlight cards in the current sequence one by one
-    const highlightSequence = async () => {
+    const highlightPattern = async () => {
       await new Promise((resolve) => setTimeout(resolve, 1000));
-      for (let i = 0; i < sequence.length; i++) {
-        setHighlightedCards([sequence[i]]);
-        await new Promise((resolve) =>
-          setTimeout(resolve, highlightDuration + 50)
-        );
-        setHighlightedCards([]);
-        await new Promise((resolve) => setTimeout(resolve, 50));
-      }
-      //setHighlightedCards([]); -> nichtmehr nötig
+      setHighlightedCards(pattern); // Highlight current cards
+      await new Promise((resolve) => setTimeout(resolve, highlightDuration));
+      setHighlightedCards([]); // Stop highlighting cards
       setInputEnabled(true);
     };
 
-    if (sequence.length > 0) {
-      highlightSequence();
+    if (pattern.length > 0) {
+      highlightPattern();
     }
-  }, [sequence]);
+  }, [pattern]);
 
   const handleCardClick = (id: number) => {
     if (inputEnabled) {
@@ -62,19 +65,17 @@ export default function SequenceMemory() {
   };
 
   useEffect(() => {
-    if (sequence.length > 0) {
+    if (pattern.length > 0) {
       for (var i = 0; i < clickedCards.length; i++) {
-        if (clickedCards[i] != sequence[i]) {
-          setIsSequenceCorrect(false);
+        //Check if the clicked card is in the pattern
+        if (!pattern.includes(clickedCards[i])) {
+          setIsPatternCorrect(false);
           break;
         }
       }
 
-      if (
-        isSequenceCorrect &&
-        clickedCards.length == sequence.length &&
-        clickedCards[0] == sequence[0]
-      ) {
+      //Go to new level ...
+      if (isPatternCorrect && clickedCards.length == pattern.length) {
         setInputEnabled(false);
         setLevel(level + 1);
         setClickedCards([]);
@@ -83,16 +84,16 @@ export default function SequenceMemory() {
   }, [clickedCards]);
 
   useEffect(() => {
-    if (!isSequenceCorrect) {
+    if (!isPatternCorrect) {
       setInputEnabled(false);
-      setIsSequenceCorrect(true);
+      setIsPatternCorrect(true);
       setHighlightedCards([]);
       setClickedCards([]);
-      setSequence([]);
+      setPattern([]);
       setLevel(0);
       router.replace(`/gameOver?title=${title}&level=${level}`);
     }
-  }, [isSequenceCorrect]);
+  }, [isPatternCorrect]);
 
   return (
     <View style={styles.container}>
@@ -108,7 +109,10 @@ export default function SequenceMemory() {
                 <MemoryCard
                   key={cardIndex}
                   id={cardIndex}
-                  isClicked={highlightedCards.includes(cardIndex)}
+                  isClicked={
+                    clickedCards.includes(cardIndex) ||
+                    highlightedCards.includes(cardIndex)
+                  }
                   disable={!inputEnabled}
                   onClick={() => handleCardClick(cardIndex)}
                 />
