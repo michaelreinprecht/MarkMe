c405931e9090e89d837d92444ef8d572f980833e
michaelreinprecht <reinprecht.michael.business@gmail.com>
2024-11-21 11:47:57 +0100

    Added ImprovementCarousel Component

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+// MainMenu.tsx
 import React, { useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import NavigationButton from "../components/NavigationButton";
@@ -6,12 +7,12 @@ import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import PageTitle from "../components/PageTitle";
 import { init } from "../localDB/DBHighscore";
+import ImprovementCarousel from "../components/ImprovementCarousel"; // Import the new carousel component
 
 SplashScreen.preventAutoHideAsync();
 
 export default function MainMenu() {
   const [loaded, error] = useFonts({
-    // Load fonts
     Jaro: require("../assets/fonts/Jaro.ttf"),
   });
 
@@ -39,22 +40,27 @@ export default function MainMenu() {
     <View style={styles.container}>
       <PageTitle text="MarkMe" fontSize={70} />
 
-      {/* Dynamically render buttons using GameModes */}
-      {Object.entries(GameModes).map(([title, { startPath, icon }]) => (
-        <NavigationButton
-          key={title} // Unique key for each button
-          text={title}
-          path={startPath}
-          icon={icon}
-        />
-      ))}
+      {/* Use the ImprovementCarousel component */}
+      <ImprovementCarousel />
 
-      {/* Add Highscores button */}
-      <NavigationButton
-        text="Highscores"
-        path="/highscores"
-        icon={require("../assets/HighScore.png")}
-      />
+      {/* Dynamically render buttons using GameModes */}
+      <View style={styles.buttonContainer}>
+        {Object.entries(GameModes).map(([title, { startPath, icon }]) => (
+          <NavigationButton
+            key={title} // Unique key for each button
+            text={title}
+            path={startPath}
+            icon={icon}
+          />
+        ))}
+
+        {/* Add Highscores button */}
+        <NavigationButton
+          text="Highscores"
+          path="/highscores"
+          icon={require("../assets/HighScore.png")}
+        />
+      </View>
     </View>
   );
 }
@@ -63,6 +69,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.background,
+    justifyContent: "flex-start", // This ensures the content is aligned at the top
     alignItems: "center",
+    paddingTop: 10, // Add a small amount of padding to the top if needed
+  },
+  buttonContainer: {
+    marginTop: 20,
+    flexGrow: 1, // Ensures the buttons take up remaining space
+    justifyContent: "space-evenly", // Aligns buttons at the top of the remaining space
+    width: "100%",
+    alignItems: "center", // Keep buttons centered horizontally
   },
 });
