7e9cfb1f829b12427f463a8fb806fed805ec848b
michaelreinprecht <reinprecht.michael.business@gmail.com>
2024-12-05 15:36:59 +0100

    Made buttons more consistent in number memory and verbal memory

--- app/number-memory-game.tsx
+++ app/number-memory-game.tsx
@@ -6,7 +6,17 @@ This page includes the actual implementation for the number memory game (includi
 */
 
 import React, { useState, useEffect } from "react";
-import {View,Text,StyleSheet,TextInput,Button,KeyboardAvoidingView,Platform, TouchableOpacity} from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 import { useRouter } from "expo-router";
 import LevelIndicator from "../components/LevelIndicator";
 import PageTitle from "../components/PageTitle";
@@ -14,12 +24,14 @@ import { Colors, Fonts } from "../constants/Constants";
 
 export default function NumberMemory() {
   const [level, setLevel] = useState(0);
-  const [sequence, setSequence] = useState<string>(""); 
-  const [userInput, setUserInput] = useState<string>(""); 
-  const [isInputEnabled, setIsInputEnabled] = useState(false); 
-  const [displayNumber, setDisplayNumber] = useState(true); 
+  const [sequence, setSequence] = useState<string>("");
+  const [userInput, setUserInput] = useState<string>("");
+  const [isInputEnabled, setIsInputEnabled] = useState(false);
+  const [displayNumber, setDisplayNumber] = useState(true);
   const router = useRouter();
   const title = "Number Memory";
+
+  const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
 
   // Duration per digit 1000ms per digit
   const durationPerDigit = 1000;
@@ -49,7 +61,7 @@ export default function NumberMemory() {
       setIsInputEnabled(true); // Enable input after the number disappears
     }, displayTime);
 
-    return () => clearTimeout(timer); 
+    return () => clearTimeout(timer);
   }, [level]);
 
   const handleSubmit = () => {
@@ -72,8 +84,10 @@ export default function NumberMemory() {
       <LevelIndicator level={level} />
 
       <View style={styles.gameArea}>
-        <Text style={styles.info}>{displayNumber ? sequence : "Enter the number"}</Text>
-        
+        <Text style={styles.info}>
+          {displayNumber ? sequence : "Enter the number"}
+        </Text>
+
         <TextInput
           style={styles.input}
           value={userInput}
@@ -84,64 +98,69 @@ export default function NumberMemory() {
           editable={isInputEnabled}
         />
 
-        <TouchableOpacity style={styles.button} onPressOut={handleSubmit}  disabled={!isInputEnabled || userInput.length === 0}>
-          <Text style={styles.buttonText}>Submit</Text>
-        </TouchableOpacity>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={[styles.button, { minHeight: screenHeight * 0.09 }]}
+            onPressOut={handleSubmit}
+            disabled={!isInputEnabled || userInput.length === 0}
+          >
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: Colors.background,
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    gameArea: {
-        alignItems: "center",
-        marginTop: 20,
-        width: "100%"
-    },
-    info: {
-      color: Colors.lightText,
-      fontSize: Fonts.sizes.medium,
-      minHeight: 20,
-      marginBottom: 10,
-      textAlign: "center",
-    },
-    input: {
-        borderColor: Colors.lightText,
-        borderWidth: 1,
-        borderRadius: 8,
-        padding: 10,
-        width: "80%",
-        fontSize: 18,
-        textAlign: "center",
-        marginBottom: 20,
-        color: Colors.lightText,
-    },
-    button: {
-      backgroundColor: Colors.buttonTertiary,
-      paddingVertical: 14,
-      paddingHorizontal: 24,
-      borderRadius: 10,
-      marginBottom: 16,
-      alignItems: "center",
-      justifyContent: "center",
-      shadowColor: "#000",
-      shadowOffset: { width: 0, height: 3 },
-      shadowOpacity: 0.15,
-      shadowRadius: 5,
-      elevation: 4,
-      width: "80%"
-    },
-    buttonText: {
-      color: Colors.lightText,
-      fontSize: 18,
-      fontWeight: "600",
-      textTransform: "uppercase",
-      letterSpacing: 1,
-    },
+  container: {
+    backgroundColor: Colors.background,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  gameArea: {
+    alignItems: "center",
+    marginTop: 20,
+    width: "100%",
+  },
+  info: {
+    color: Colors.lightText,
+    fontSize: Fonts.sizes.medium,
+    minHeight: 20,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  input: {
+    borderColor: Colors.lightText,
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 10,
+    width: "80%",
+    fontSize: 18,
+    textAlign: "center",
+    marginBottom: 20,
+    color: Colors.lightText,
+  },
+  buttonText: {
+    color: Colors.lightText,
+    fontSize: Fonts.sizes.large,
+    fontFamily: Fonts.family.button,
+  },
+  buttonContainer: {
+    gap: 10,
+    width: "80%",
+  },
+  button: {
+    backgroundColor: Colors.buttonSecondary,
+    justifyContent: "center",
+    flexDirection: "row", // Allows icon and text to be side by side
+    alignItems: "center",
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: Colors.buttonBorder,
+    flexGrow: 1,
+  },
 });
